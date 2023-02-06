d644f63b0722900e8924b69168441594db241bee
kienkhang <trankienkhang2210@gmail.com>
2023-02-06 18:15:13 +0700

    feat: using custom pinia

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,21 +4,22 @@
     <div class="text-lg font-bold">Count : {{ count }}</div>
     <div class="text-lg font-bold">Double Count : {{ doubleCount }}</div>
     <button class="p-2 rounded-md bg-green-600 text-white" @click="increment">Increment</button>
+    <!-- <button class="p-2 rounded-md bg-blue-600 text-white" @click="disposeCounter">
+      DisposeCounter
+    </button> -->
     <button class="p-2 rounded-md bg-red-600 text-white" @click="navigate">Go to 404</button>
     <HelloWorld :msg="'Khang'"></HelloWorld>
   </div>
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import useCounterStore from '@/stores/counter'
 const router = useRouter()
 
 const store = useCounterStore()
 
-const { count, doubleCount } = storeToRefs(store)
-const { increment } = store
+const { increment, count, doubleCount } = store
 
 const navigate = () => {
   router.push('/')
