9edae7facf9d1d8130b1b9b6c1432a93c4680cec
KienKhang <trankienkhang2210@gmail.com>
2023-02-06 10:49:01 +0700

    feat: testing component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,12 @@
 <template>
   <div>Không tìm thấy thông tin</div>
   <button @click="navigate">Go to Home Page</button>
+  <TheWelcome></TheWelcome>
 </template>
 
 <script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+
 const router = useRouter()
 const navigate = () => {
   router.push('home')
