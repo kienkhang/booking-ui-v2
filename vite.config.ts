3ba7a3d435d8d4cfbeedde495b3ed8ed806faaea
kienkhang <trankienkhang2210@gmail.com>
2023-02-08 16:40:42 +0700

    feat: VueRouter Auto and Unplugin Icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
 import Layouts from 'vite-plugin-vue-layouts'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+// import Pages from 'vite-plugin-pages'
+import VueRouter from 'unplugin-vue-router/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+// Icon
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,12 +22,27 @@ export default defineConfig({
   },
 
   plugins: [
-    vue(),
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages({
-      extensions: ['vue', 'md'],
-      exclude: ['**/components/*.vue'],
+    // https://github.com/posva/unplugin-vue-router
+    VueRouter({
+      // Allow .vue and .ts file access
+      extensions: ['.vue', '.ts'],
+      dts: 'src/typed-router.d.ts',
     }),
+    vue(),
+    // https://github.com/antfu/unplugin-icons
+    Icons({
+      compiler: 'vue3',
+      customCollections: {
+        local: FileSystemIconLoader('./src/assets/icons', (svg) =>
+          svg.replace(/^<svg /, '<svg fill="currentColor" ')
+        ),
+      },
+    }),
+    // https://github.com/hannoeru/vite-plugin-pages
+    // Pages({
+    //   extensions: ['vue', 'md'],
+    //   exclude: ['**/components/*.vue'],
+    // }),
 
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
     Layouts(),
@@ -30,13 +51,15 @@ export default defineConfig({
     AutoImport({
       imports: [
         'vue',
-        'vue-router',
         'vue-i18n',
         'vue/macros',
         '@vueuse/head',
         '@vueuse/core',
+        // 'vue-router',
+        VueRouterAutoImports,
         {
           pinia: ['defineStore'],
+          'vue-router/auto': ['useLink'],
         },
       ],
       dts: 'src/auto-imports.d.ts',
@@ -51,6 +74,12 @@ export default defineConfig({
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
       dts: 'src/components.d.ts',
+      resolvers: [
+        IconsResolver({
+          prefix: 'i',
+          customCollections: ['local'],
+        }),
+      ],
     }),
   ],
 })
