2914179d3cc937f6bf3f47751003c68f514cd1e8
kienkhang <trankienkhang2210@gmail.com>
2023-02-08 13:43:23 +0700

    feat: basic login page

---
+++ src/pages/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <TheWelcome></TheWelcome>
+  <button @click="navigate" class="p-2 bg-green-500 textwhite">Go to Home Page</button>
+</template>
+
+<script setup lang="ts">
+import TheWelcome from '@/components/TheWelcome.vue'
+const router = useRouter()
+const navigate = () => {
+  router.push('home')
+}
+</script>
+
+<route lang="yaml">
+meta:
+  layout: default
+</route>
