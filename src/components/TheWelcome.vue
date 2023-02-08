23da59c8435eed15a92dc7dc9d47d3403fc00539
kienkhang <trankienkhang2210@gmail.com>
2023-02-08 13:42:45 +0700

    feat: add login flow

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,16 +1,26 @@
 <template>
-  <div v-if="isLoggedIn" class="text-green-700 font-bold">Have Account</div>
-  <FormKit type="form" submit-label="Login" @submit="onLogin">
+  <FormKit type="form" @submit="onLogin" :actions="false">
     <FormKit type="text" label="Email" name="email" validation="email|required"></FormKit>
     <FormKit type="password" label="Password" name="password" validation="required"></FormKit>
+    <FormKit
+      name="login"
+      :label="!isLoading ? 'Login' : 'X'"
+      :class="isLoading && 'animate-spin'"
+      type="submit"
+    ></FormKit>
+
+    <FormKit @click="logout" name="logout" type="button">Logout</FormKit>
   </FormKit>
 </template>
 
 <script setup lang="ts">
-const { login } = useAuth()
-const { isLoggedIn } = useAccountStorage()
-const onLogin = ({ email = '', password = '' }) => {
-  const { isLoading, executeAPI } = login({ email, password })
+const { login, logout } = useAuth()
+
+const loginForm = reactive({ email: '', password: '' })
+const { isLoading, executeAPI } = login(loginForm)
+const onLogin = (data: { email: string; password: string }) => {
+  loginForm.email = data.email
+  loginForm.password = data.password
   executeAPI()
 }
 </script>
