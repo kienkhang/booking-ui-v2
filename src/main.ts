f169a04e529d5c1afdf3ac76767830082eda49ea
kienkhang <trankienkhang2210@gmail.com>
2023-02-08 16:44:12 +0700

    feat: convert vitepluginpage to vuerouter auto

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,17 @@ import { ViteSSG } from 'vite-ssg'
 import { setupLayouts } from 'virtual:generated-layouts'
 import App from './App.vue'
 import type { UserModule } from './types'
-import generatedRoutes from '~pages'
+// Vite Plugin Page (old)
+// import generatedRoutes from '~pages'
+// const routes = setupLayouts(generatedRoutes)
+// VueRoute (new)
+import { routes } from 'vue-router/auto/routes'
 
-const routes = setupLayouts(generatedRoutes)
-
+// Tailwind CSS
 import './styles/index.css'
 
 // https://github.com/antfu/vite-ssg
-export const createApp = ViteSSG(App, { routes }, (ctx) => {
+export const createApp = ViteSSG(App, { routes: setupLayouts(routes) }, (ctx) => {
   // install all modules under `modules/`
   Object.values(
     import.meta.glob<{ install: UserModule }>('./modules/*.ts', {
