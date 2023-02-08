ba295f4f9a0d3c05a4dd6f0758c6e65806c2c3bd
kienkhang <trankienkhang2210@gmail.com>
2023-02-08 13:41:07 +0700

    feat: add logout and get profile

--- src/composables/useAuth.ts
+++ src/composables/useAuth.ts
@@ -1,34 +1,55 @@
 import factory from '@/api/factory'
 import useAuthStore from '@/stores/auth'
-
 export default function useAuth() {
   // Import
   const api = factory.get('auth')
   const { setAuth } = useAuthStore()
-  const { setToken } = useAccountStorage()
-
+  const { setToken, removeToken } = useAccountStorage()
   // method
-  const login = ({ email = '', password = '' }) => {
-    const usedLogin = api.login({ email, password })
-    const { data, isFinished, error, execute } = usedLogin
+  // --------------------------------
+  const login = (data?: { email: string; password: string }) => {
+    const usedLogin = api.login({})
+    const { data: respData, isFinished, error, execute } = usedLogin
 
     until(isFinished)
       .toBeTruthy()
       .then(() => {
         if (!!error.value === false) {
-          setAuth(data.value)
-          setToken(data?.value?.token?.access_token)
+          setAuth(respData.value)
+          setToken(respData?.value?.token?.access_token)
         } else {
-          throw new Error('Lỗi khi gọi API')
+          throw new Error(error.value?.response?.data.message!)
         }
       })
     return {
       ...usedLogin,
+      executeAPI: () => execute({ data }),
+    }
+  }
+  // --------------------------------
+  const getProfile = () => {
+    const usedGetme = api.getMe()
+    const { data, isFinished, execute } = usedGetme
+
+    until(isFinished)
+      .toBeTruthy()
+      .then(() => {
+        setAuth(data.value)
+      })
+    return {
+      ...usedGetme,
       executeAPI: () => execute(),
     }
+  }
+  // --------------------------------
+  const logout = () => {
+    setAuth(null)
+    removeToken()
   }
 
   return {
     login,
+    logout,
+    getProfile,
   }
 }
